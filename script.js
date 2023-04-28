107cbbf2504becd6ee81905384de9d852bf549cd
Pasindu-Jayath <61136311+Pasindu-Jayath@users.noreply.github.com>
2023-04-28 09:47:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let items = document.querySelectorAll(".carousel .carousel-item");
+
+items.forEach((el) => {
+  const width = window.screen.width;
+  console.log(window.screen.width);
+  let minPerSlide = 4;
+
+  let next = el.nextElementSibling;
+  for (var i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      next = items[0];
+    }
+    let cloneChild = next.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
